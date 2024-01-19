6c6663be2a09472c8897787c219bf26a06b76e81
streakingman <z_max_y@163.com>
2024-01-19 12:14:00 +0800

    chore: vant

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: '杭州星瀚投资管理有限公司',
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                { name: 'description', content: '123' },
+            ],
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        },
+    },
+    modules: ['@vant/nuxt'],
     devtools: { enabled: true },
 });
