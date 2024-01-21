cb5d03096d62f96495f24ea781ce16d2bccef3d8
streakingman <z_max_y@163.com>
2024-01-21 12:39:19 +0800

    feat: 招贤纳士

--- pages/join-us.vue
+++ pages/join-us.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { jobs } from '~/site-data/jobs';
+</script>
 
-<template>join-us</template>
+<template>
+    <template v-for="(job, idx) of jobs" :key="idx">
+        <h2 class="font-bold text-2xl text-center">{{ job.title }}</h2>
+        <Card
+            v-for="({ title, content }, i) of job.desc"
+            :key="i"
+            :title="title"
+            :content="content"
+        />
+    </template>
+</template>
 
 <style scoped></style>
