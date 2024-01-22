d3922428e39a0e2a1db321164c715210f7fe28c8
streakingman <z_max_y@163.com>
2024-01-22 13:49:11 +0800

    feat: 产品根据sort字段排序

--- server/api/product.get.ts
+++ server/api/product.get.ts
@@ -1,7 +1,9 @@
 import { ProductModel } from '~/server/models/product';
 
 export default defineEventHandler(async () => {
-    const products = await ProductModel.find({});
+    const products = await ProductModel.find({}).sort({
+        sort: 1,
+    });
 
     products.forEach((p: any) => {
         p.timeTend = (p.timeTend || []).map((i: any) => i.toLocaleDateString());
