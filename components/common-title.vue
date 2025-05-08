1f8faa82922bf5eba4616e63c2380127c141cfc0
jiye111 <39900303+jiye111@users.noreply.github.com>
2025-05-08 20:18:25 +0800

    Update common-title.vue

--- components/common-title.vue
+++ components/common-title.vue
@@ -4,15 +4,13 @@
 
 <template>
     <div
-        class="h-10 text-white text-sm px-5 relative flex items-center justify-between overflow-hidden bg-cover mb-2"
+        class="h-10 text-white text-sm px-5 relative flex items-center justify-center overflow-hidden bg-cover mb-2"
         :style="{
             backgroundImage: 'url(/star-sky.png)',
         }"
     >
-        <span class="h-2 w-2 bg-white rotate-45"></span>
         <span class="relative">
             <slot />
         </span>
-        <span class="h-2 w-2 bg-white rotate-45"></span>
     </div>
 </template>
